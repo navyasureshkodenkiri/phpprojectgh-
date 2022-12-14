899b1a02b3c5a858765ffffc9af33be6530d6c7a
navyasureshkodenkiri <114465047+navyasureshkodenkiri@users.noreply.github.com>
2022-12-14 11:02:11 +0530

    new text added

---
+++ example1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
+</head>
+  
+<body>
+    <center>
+        <h1>Welcome To GFG</h1>
+  
+        <h2>
+            <?php 
+            echo "This is PHP code inside html";
+            echo "new one added"
+            ?>
+        </h2>
+    </center>
+</body>
+  
+</html>
