de59c2c3f99e14945c2273fc7950b6d46cd8d15c
navyasureshkodenkiri <114465047+navyasureshkodenkiri@users.noreply.github.com>
2022-12-14 15:43:44 +0530

    frist comit

---
+++ script2.php
@@ -0,0 +1,26 @@
+<?php
+     include("script1.php");
+    
+     echo "<hr>";
+     echo "visit to ecowebhosting page";
+     echo file_get_contents ("https://www.ecowebhosting.co.uk");
+
+
+
+     
+    
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+
+//echo "visit to w3school";
+     //echo file_get_contents ("https://www.w3school.com");
