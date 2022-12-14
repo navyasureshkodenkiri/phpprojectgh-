2871fa11d66b873c6400338f877e749590c98012
navyasureshkodenkiri <114465047+navyasureshkodenkiri@users.noreply.github.com>
2022-12-14 12:28:43 +0530

    multi php tag

---
+++ .history/names_20221214121901.php
@@ -0,0 +1,23 @@
+<?php
+$names = ['sowmya','havish','ramya'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="">
+    <h1>
+        <ul>
+            <?php foreach($names as $names) { ?>
+                <li><?= $names ?></li>
+
+            }
+        </ul>
+    </h1>
+    
+</body>
+</html>
